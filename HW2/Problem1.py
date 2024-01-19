6664064b7c007b384a17d0d87449e862de9d1c54
richie781 <157099082+richie781@users.noreply.github.com>
2024-01-18 23:04:52 -0800

    Create Problem1.py

---
+++ HW2/Problem1.py
@@ -0,0 +1,13 @@
+# Problem 1
+#
+import numpy as np
+
+# Problem 1a
+#
+ans1 = np.sqrt((1 + np.tan(np.exp(2 + np.pi / 2))))
+print(ans1)
+
+# Problem 1b
+#
+ans2 = np.tan(np.exp(3) + np.arcsin(np.pi / 21))
+print(ans2)
